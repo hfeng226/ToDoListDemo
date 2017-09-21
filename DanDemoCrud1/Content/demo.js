5842c78ec45139c56d24b44377cc51d9234b18f1
Helen Feng <hfeng226@gmail.com>
2017-09-21 12:09:37 -0700

    wip

---
+++ DanDemoCrud1/Content/demo.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('DemoApp', []);
+
+    app.controller('DemoController', DemoController);
+
+    DemoController.$inject = ['$http'];
+
+    function DemoController($http) {
+        var vm = this;
+      //  console.log(this);
+        $http({
+            method: 'GET',
+            url: '/api/demo/message'
+        }).then(success, error);
+        
+        function success(response) {
+            // vm.messages = response.data;
+            vm.messages = response.data
+
+        }
+        function error(error) {
+            console.log(error);
+        }
+
+    }
+
+})();
